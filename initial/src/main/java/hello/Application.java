8b6dc4aa36835fa1dd2ecee794284f068f945bf4
madhavi2491 <53891476+madhavi2491@users.noreply.github.com>
2020-09-30 19:30:16 +0530

    Update Application.java

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,11 +2,20 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @SpringBootApplication
+    @RestController
+    public class Application {
+
+        @RequestMapping
+        public String home(){
+            return "<h1>Spring Boot Hello World!</h1>";
+        }
+        
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
 
 }
